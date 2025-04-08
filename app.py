0553288c35fa0030bd2d9c5b92ac84c49ca2a832
vikasmishra1691 <vikasmishra1691@gmail.com>
2025-04-08 13:58:47 +0530

    fourth commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.append(nltk_data_dir)
 
 # Always download resources at startup
-nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('punkt_tab', download_dir=nltk_data_dir)
 nltk.download('stopwords', download_dir=nltk_data_dir)
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
